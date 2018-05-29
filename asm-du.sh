f7a36e44fb19b27a36236047f89e53c69d532e4d
Andrea de Palo <andrea.de.palo@oracle.com>
2018-05-29 10:21:28 +0200

    Added exclusion list

--- asm-du.sh
+++ asm-du.sh
@@ -8,13 +8,22 @@
 asmcmd lsdg *C1
 echo -e "\n"
 
+# list of DBs to be excluded from the output, if present
+execution_directory=`dirname $0`
+
+if [[ -f $execution_directory/asm-du.exception ]]; then
+    db_exception=`cat $execution_directory/asm-du.exception`
+fi
+
 echo "DATABASE_NAME,DATAC1_MB_USAGE,RECOC1_MB_USAGE"
 crsctl stat res -t | grep '\.db$' | while read output_line; do
     dbname=`echo $output_line | awk -F"." '{ print $2 }'`
     uc_dbname=${dbname^^}
     #echo $uc_dbname
-    datac1_usage=`asmcmd du --suppressheader DATAC1/$uc_dbname|awk -F" " '{print $1}'`
-    recoc1_usage=`asmcmd du --suppressheader RECOC1/$uc_dbname|awk -F" " '{print $1}'`
-    #asmcmd du RECOC1/$uc_dbname
-    echo "$uc_dbname,$datac1_usage,$recoc1_usage"
+    if [[ $db_exception != *"$uc_dbname"* ]]; then
+        datac1_usage=`asmcmd du --suppressheader DATAC1/$uc_dbname|awk -F" " '{print $1}'`
+        recoc1_usage=`asmcmd du --suppressheader RECOC1/$uc_dbname|awk -F" " '{print $1}'`
+        #asmcmd du RECOC1/$uc_dbname
+        echo "$uc_dbname,$datac1_usage,$recoc1_usage"
+    fi
 done
